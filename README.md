f3dfa7147c0f40935bca57010fb4137d71162a3a
tt <tt@hoge.com>
2018-03-06 23:10:39 +0900

    [refactor] web api [add] readme.md

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Purpose
 + 複数のパラメータ最適化アルゴリズムをJsonを用いたWeb APIで使用可能
++ 実行時イメージは下図。オプティマイズループが実行ライブラリの外側へ来る
++ そのため、関数に出来ないような対象も最適化アルゴリズムで実行することができる
+![](imgs/StructureSummary.PNG)
 
 ## Requirements
+hyperoptが新しいnetworkxに対応していないため、古いVersionをインストールする必要があります（2018/3現在）
 + networkx 1.11
 + hyperopt 0.1
 + GPy 1.8.5
@@ -13,22 +17,90 @@
 ## Contain Algorithms
 ### hypoeropt
 + Tree-structured Parzen Estimator
-+ annealing
-+ random
-### gpyopt (Bayesian Optimization)
-models
-+ EI
-+ MPI
-+ LCB
++ Annealing
++ Random Search
 
-aquisitions
+### gpyopt (Bayesian Optimization)
+#### models
 + GP
 + GP_MCMC
 + Sparse GP
 
-## Tutorial
+#### aquisitions
++ EI
++ MPI
++ LCB
 
 ## Web API I/F
 
-## 
+|GET|
+|:---|
+|Tutorial参照|
 
+|POSTデータ(JSON)|
+|:---|:---|:-:|:---|:---|:-:|
+|JSON Key|型|必須|概要|ライブラリ依存|
+|seed|数値||乱数シード　再現性確認用||
+|lib|文字列|◯|ライブラリ名||
+|algo|文字列|◯|ライブラリが持つアルゴリズム選択|◯|
+|scope|配列[オブジェクト]|◯|探索空間定義||
+|scope - 変数名|オブジェクト|◯|探索空間定義|◯|
+|max_evals|数値||応答数指定||
+|results|オブジェクト||過去実行結果||
+|results - losses|配列||目標値履歴||
+|results - statuses|配列||状態値(未使用)||
+|results - vals|オブジェクト||実行パラメータ履歴||
+|results - vals - 変数名|配列||scopeで定義した変数名毎の実効値|||
+
+
+|返却データ(JSON)|
+|:---|:-:|:---|
+|JSON Key|型|概要|
+|vals|オブジェクト|推奨パラメータ|
+|vals - 変数名|オブジェクト|scopeで定義した変数名毎の推奨値|
+|statuses|配列|ステータス文字列(現状"new"固定)|
+
+
+
+
+### hyperopt依存部
++ uniform
+        "scope": [
+          {ラベル名: [uniform, 下限値, 上限値]}
+        ]
++ quniform
+        "scope": [
+          {ラベル名: [quniform, 下限値, 上限値, 分解能]}
+        ]
+      戻り値はround(uniform(low, high) / q) * q
++ choice
+        "scope": [
+          {ラベル名: [choice, 選択A, 選択B, ...]}
+        ]
+[その他](https://github.com/hyperopt/hyperopt/wiki/FMin)
+
+### gpyopt依存部
++ continuous
+        "scope": [
+          {ラベル名: [continuous, 下限値, 上限値]}
+        ]
++ discrete
+        "scope": [
+          {ラベル名: [discrete, a, b, c]}
+        ]
+[その他](http://sheffieldml.github.io/GPyOpt/tutorial/index.html)
+
+
+## Tutorial
+*Web service起動*
+```
+python param_opt_web.py
+```
+*Optimize 実行*
+```
+http://127.0.0.1:5000/suggest?condition={"seed":0,"lib":"hyperopt","algo":"rand","scope":[{"x":["uniform",-10,10]},{"y":["uniform",-10,10]}],"max_evals":3,"results":{"losses":[],"statuses":[],"vals":{"y":[], "x":[]}}}
+```
+
+## LICENSE
+[hyperopt](https://github.com/hyperopt/hyperopt/blob/master/LICENSE.txt)
+[gpyopt](https://github.com/SheffieldML/GPyOpt/blob/master/LICENSE.txt)
--- param_opt_web.py
+++ param_opt_web.py
@@ -3,7 +3,7 @@ sys.path.append(".")
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
 import json
-from opts.optexecutor import OptExecutorFactory
+from opts.optexecutor_factory import OptExecutorFactory
 from utils.checker_jsonstructure import WrongStructureException
 
 # instance name:param_opt_web
@@ -21,20 +21,19 @@ def picked_up():
 # index にアクセスしたときの処理
 @param_opt_web.route('/')
 def index():
-    title = "Hyper Parameter Search by Tree-parzen Estimator"
+    title = "Hyper Parameter Search"
     message = picked_up()
     # index.html をレンダリングする
     return render_template('index.html',
                            message=message, title=title)
 
-# http://127.0.0.1:5000/suggest?condition=hoge
+
+# http://127.0.0.1:5000/suggest?condition={"seed":0,"lib":"hyperopt","algo":"rand","scope":[{"x":["uniform",-10,10]},{"y":["uniform",-10,10]}],"max_evals":3,"results":{"losses":[3.4777,3.294,28.729,19.62,20.458],"statuses":["ok","ok","ok","ok","ok"],"vals":{"y":[-0.13974,0.3722,-2.419,0.28755,-3.2827],"x":[1.8571,1.765,4.615,-4.068,2.832]}}}
+# http://127.0.0.1:5000/suggest?condition={"seed":0,"lib":"hyperopt","algo":"rand","scope":[{"x":["uniform",-10,10]},{"y":["uniform",-10,10]}],"max_evals":3,"results":{"losses":[],"statuses":[],"vals":{"y":[], "x":[]}}}
 @param_opt_web.route('/suggest', methods=['GET', 'POST'])
 def suggest():
     if request.method == 'GET':
         condition = request.args.get('condition')
-        # print(condition)
-        # condition = '{"seed":0,"lib":"hyperopt","algo":"tpe","scope":{"x":["uniform",-10,10],"y":["uniform",-10,10]},"max_evals":1,"results":{"losses":[3.4777,3.294,28.729,19.62,20.458],"statuses":["ok","ok","ok","ok","ok"],"vals":{"y":[-0.13974,0.3722,-2.419,0.28755,-3.2827],"x":[1.8571,1.765,4.615,-4.068,2.832]}}}'
-        # print(condition)
     elif request.method == 'POST':
         condition = request.json
     else:
@@ -50,14 +49,12 @@ def suggest():
 
         rval = dict(statuses=[err.args])
 
-
     render_template('index.html', title='Parameter Optimizer Condition', message=rval)
     print(type(rval))
     return json.dumps(rval)
 
 
 if __name__ == '__main__':
-    param_opt_web.debug = True # デバッグモード有効化
-    # param_opt_web.debug = False
-    # param_opt_web.run(host='127.0.0.1')
-    param_opt_web.run(host='JP04990730')
+    # param_opt_web.debug = True # デバッグモード有効化
+    param_opt_web.debug = False
+    param_opt_web.run(host='127.0.0.1')
